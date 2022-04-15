97cd30b78848851d619847e2368c73d2f5fd2c45
Athirson <Athirsonarceus@gmail.com>
2022-04-15 15:55:45 -0300

    Player created

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import time
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
 
+from car_manager import CarManager
+from player import Player
+from scoreboard import Scoreboard
+from turtle import Screen
+
+# Creating the screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+# Creating the player
+player = Player()
+
+# Listen to keystroke
+screen.listen()
+screen.onkey(player.up, 'w')
+screen.onkey(player.up, 'Up')
+
+
 game_is_on = True
+loops = 1
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    loops += 1
+    if loops % 6 == 0:
+        pass
+
+    print(loops)
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    """ Making the player model class """
+
+    def __init__(self):
+        super().__init__()
+        self.shape('turtle')
+        self.penup()
+        self.color('black')
+        self.setheading(90)
+        self.speed('fast')
+        self.goto(0, -300)
+
+    def up(self):
+        """ Move the paddle up """
+        new_y = self.ycor() + 20
+        self.goto(x=self.xcor(), y=new_y)
