6665be26665858e82014ce2d293f8d21d0e905e3
Athirson <Athirsonarceus@gmail.com>
2022-04-15 17:58:40 -0300

    Car render

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,38 @@
+from random import choice
+from random import randint
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    """ Class responsible to generate the random """
+
+    def __init__(self):
+        super().__init__()
+        self.all_cars = list()
+        self.car_speed = MOVE_INCREMENT
+
+    def create_car(self):
+        """" Creates the car """
+
+        random_chance = randint(1, 3)
+        if random_chance == 1:
+            new_car = Turtle('square')
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(choice(COLORS))
+            random_y = randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        """ Moves the cars """
+
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
